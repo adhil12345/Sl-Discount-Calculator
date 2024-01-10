5626d984155c5f088ab83ee26833d80930afd901
adhil12345 <84992437+adhil12345@users.noreply.github.com>
2024-01-10 19:47:10 +0530

    Update Srilanka-Discount-Calculator.js

--- Srilanka-Discount-Calculator.js
+++ Srilanka-Discount-Calculator.js
@@ -1,14 +1,1 @@
-function calculateDiscount() {
-        var originalPrice = parseFloat(document.getElementById('originalPrice').value);
-        var discountPercentage = parseFloat(document.getElementById('discountPercentage').value);
-
-        if (!isNaN(originalPrice) && !isNaN(discountPercentage)) {
-            var discountAmount = (originalPrice * discountPercentage) / 100;
-            var discountedPrice = originalPrice - discountAmount;
-
-            document.getElementById('result').innerHTML = "Discount Amount: LKR" + discountAmount.toFixed(2) +
-                "<br>Discounted Price: LKR" + discountedPrice.toFixed(2);
-        } else {
-            document.getElementById('result').innerHTML = "Please enter valid numbers.";
-        }
-    }
+document.getElementById("demo").innerHTML = "My First JavaScript";
